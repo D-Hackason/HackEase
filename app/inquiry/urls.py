c8a9f57d05d737d04293dd41cc2418623112dc3a
mo-ri-regen <moricontb@gmail.com>
2025-11-09 14:50:36 +0900

    質問フォーム作成

---
+++ app/inquiry/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.question_form, name='question_form'),
+    path('/test', views.index, name='inquiry_index'),
+]
--- app/inquiry/views.py
+++ app/inquiry/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import QuestionForm
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Inquiry app is working!")
+
+def question_form(request):
+    return render(request, 'inquiry/question_form.html')
+
+def question_view(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'inquiry/thanks.html')
+    else:
+        form = QuestionForm()
+
+    return render(request, 'inquiry/question_form.html', {'form': form})
