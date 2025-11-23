21d300deba7b44d56a2b69134acf0e3e1f773164
mo-ri-regen <moricontb@gmail.com>
2025-11-23 13:48:07 +0900

    #5 送信ボタンを押したときにデータを保存するように修正

--- app/inquiry/urls.py
+++ app/inquiry/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "inquiry" 
+
 urlpatterns = [
     path('', views.question_form, name='question_form'),
-    path('/test', views.index, name='inquiry_index'),
+    path("success/", views.success, name="success"),
 ]
--- app/inquiry/views.py
+++ app/inquiry/views.py
@@ -3,19 +3,23 @@ from .forms import QuestionForm
 
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Inquiry app is working!")
-
 def question_form(request):
-    return render(request, 'inquiry/question_form.html')
-
-def question_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
-            form.save()
-            return render(request, 'inquiry/thanks.html')
+            q = form.save(commit=False)
+
+            # ログインユーザーだけ user を設定する
+            if request.user.is_authenticated:
+                q.user = request.user            
+            
+            q.save()
+            return redirect('inquiry:success')
     else:
         form = QuestionForm()
-
+    
     return render(request, 'inquiry/question_form.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'inquiry/success.html')
